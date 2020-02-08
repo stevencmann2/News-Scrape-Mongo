4180e21dea42c60dfd7d35bc1ce2066081f682f5
Steven Mann <stevencmann2@gmail.com>
2020-02-07 22:57:24 -0600

    adding axios, cheerio to controller for now

--- controller/newsScrapeController.js
+++ controller/newsScrapeController.js
@@ -4,10 +4,57 @@ const router = express.Router();
 // const db = require('../models')
 
 
+//SCRAPING
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// First, tell the console what server.js is doing
+////MVP:
+/////////////// TITLE, SUMMARY, URL 
+//OPTIONAL -- photos etc. 
+console.log("\n***********************************\n" +
+            "Grabbing every title and link\n" +
+            "from reverb:" +
+            "\n***********************************\n");
+
+
+
+axios.get("https://reverb.com/").then(function(response) {
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  const $ = cheerio.load(response.data);
+ 
+  let results = [];
+
+  $("li.tiles__tile").each(function(i, element) {
+    
+    // console.log(element)
+    let title = $(element).children().text();
+    let link = $(element).find("a").attr("href");
+
+    
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  console.log(results);
+});
+
+
+
+////// CUSTOM JS PASSED THHROUGH HANDLEBARS
 var indexJs= ('/assets/js/index.js');
 var savedJs= ('/assets/js/saved.js');
 
 
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+///////////////////        ROUTING      //////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
 // Homepage
 router.get('/', function (req, res, next) {
