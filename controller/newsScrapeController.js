c99af3f3bf3d7be6ed6711a8ae14bc011a66f0b4
Steven Mann <stevencmann2@gmail.com>
2020-02-07 20:43:38 -0600

    added footer; more styling; front end JS render through Handlebars

--- controller/newsScrapeController.js
+++ controller/newsScrapeController.js
@@ -4,12 +4,17 @@ const router = express.Router();
 // const db = require('../models')
 
 
+var indexJs= ('/assets/js/index.js');
+var savedJs= ('/assets/js/saved.js');
+
+
 
 // Homepage
 router.get('/', function (req, res, next) {
 
     res.render('index', {
         title: 'Scrape Recent News',
+        indexScript: indexJs
 
     });
 });
@@ -19,6 +24,7 @@ router.get('/saved', function (req, res, next) {
 
     res.render('saved', {
         title: 'Your Saved Articles',
+        savedScript: savedJs 
     });
 });
 
