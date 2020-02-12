61a2e936fc6f69c1042e8f18a76678dc6f6cbda1
Steven Mann <stevencmann2@gmail.com>
2020-02-11 18:20:16 -0600

    deleting comment; linking heroku

--- models/Article.js
+++ models/Article.js
@@ -30,9 +30,7 @@ const ArticleSchema = new Schema({
     type: Boolean,
     default: false
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
+  
   note: [
     {type: Schema.Types.ObjectId,
     ref: "Note"}
